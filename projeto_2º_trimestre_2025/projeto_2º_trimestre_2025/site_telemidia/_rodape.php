fd0699522d18527faaf9d05bf6f2515a14a7e54f
Juliano <72839366+julianocgc@users.noreply.github.com>
2025-05-11 17:19:40 -0300

    Envio de códigos
    Envio de códigos da faculdade.

---
+++ projeto_2º_trimestre_2025/projeto_2º_trimestre_2025/site_telemidia/_rodape.php
@@ -0,0 +1,19 @@
+<!-- rodapé da página -->
+<footer>
+<div id="rodape">
+	<div id="Email">
+    <div align="center">sac@telemidia.net.br</div>
+    </div>
+    <div id="contato">
+    <div align="left">0800-283-8080 | WhatsApp: (35) 3731-1118</div>
+    </div>
+    <div id="endereco"> Rua Tutóia nº 65 | Poços de Caldas - MG </div>
+  </div>
+<div id="rodape1">
+	Copyright © 1994 Telemidia - Todos os direitos reservados</div>
+
+  <p class="style5">Desenvolvido por Juliano Guimarães Corrêa | Publicado em 25/05/2025 </p>
+</footer>
+<script src="./js/script.js"></script>
+</body>
+</html>
